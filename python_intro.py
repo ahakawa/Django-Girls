f634cfcba6993820bc90c56bd3b5140a64d45139
Rose Oyakawa <rose.oyakawa@gmail.com>
2019-12-07 14:47:11 -0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,39 @@
+# name = 'Esther'
+
+# if name == 'Ola':
+#     print('Ola Ola!')
+
+# elif name == 'Sonja':  # elif é o mesmo que else if 
+#     print('Ola Sonja')
+
+# else:
+#     print('Ola estranho')
+
+# def hi(name):
+#     if name == 'Ola':
+#         print('Ola Ola!')
+
+#     elif name == 'Sonja':  # elif é o mesmo que else if 
+#         print('Ola Sonja')
+
+#     else:
+#         print('Ola estranho')
+
+# hi("Vera")
+
+
+# def hi(name):
+#     print("Olá " + name + '!')
+
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'voce']
+
+# for name in girls:
+#     hi(name)
+#     print('Próxima')
+
+for i in range(1, 6):
+    print(i)
+
+
+
+
